22ff740f4ded6d13da69a85e5d329894d87d1838
T154N <santiiariias2001@gmail.com>
2024-05-26 23:00:14 -0300

    comiit 2

--- PPAI_Reentrega/src/main/java/Clases/Bodega.java
+++ PPAI_Reentrega/src/main/java/Clases/Bodega.java
@@ -7,8 +7,59 @@ public class Bodega {
     private String nombre;
     private Object periodoActualizacion;
 
-    public Bodega(){
-    };
+    public Bodega() {
+    }
 
+    public double getCoordenadasUbicacion() {
+        return this.coordenadasUbicacion;
+    }
+
+    public void setCoordenadasUbicacion(double coordenadasUbicacion) {
+        this.coordenadasUbicacion = coordenadasUbicacion;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getHistoria() {
+        return this.historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Object getPeriodoActualizacion() {
+        return this.periodoActualizacion;
+    }
+
+    public void setPeriodoActualizacion(Object periodoActualizacion) {
+        this.periodoActualizacion = periodoActualizacion;
+    }
+
+
+    public void contarResenia() {
+    }
+
+    public void mostrarTodosVinos() {
+    }
+
+    public void disponibleActualizar() {
+    }
+
+    public void actualizarDatosVino() {
+    }
 }
-
