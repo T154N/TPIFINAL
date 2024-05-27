cc7962ad11b281e130faec948fe502eb7915738d
T154N <santiiariias2001@gmail.com>
2024-05-26 22:54:31 -0300

    comiit 1

---
+++ PPAI_Reentrega/src/main/java/Clases/Bodega.java
@@ -0,0 +1,14 @@
+package Clases;
+
+public class Bodega {
+    private double coordenadasUbicacion;
+    private String descripcion;
+    private String historia;
+    private String nombre;
+    private Object periodoActualizacion;
+
+    public Bodega(){
+    };
+
+}
+
