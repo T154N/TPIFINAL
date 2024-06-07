86e4a1f54d13dc4b42958f0d2daddbfdfebc6fdf
Mateo <mateobringas6@gmail.com>
2024-06-06 22:48:04 -0300

    Tabla no se llena

--- PPAI_Reentrega/src/main/java/Controladores/ControladorImportarActualizacionVinosBodega.java
+++ PPAI_Reentrega/src/main/java/Controladores/ControladorImportarActualizacionVinosBodega.java
@@ -20,16 +20,17 @@ public class ControladorImportarActualizacionVinosBodega {
 	private List<String> nombresDeUsuariosSeguidores;
 	private List<Bodega> listaBodegas;
 
+	private PantallaAB pantalla;
+
 
 
 	//CREAR BODEGAS DE PRUEBA
 	public void crearBodegasDePrueba() {
-
-		listaBodegas.add(new Bodega("123.45", "Descripcion 2", "Historia 1", "Bodega 1", 4, "2025-04-01", null, null));
-		listaBodegas.add(new Bodega("123.45", "Descripcion 3", "Historia 1", "Bodega 2", 2, "2025-04-01", null, null));
-		listaBodegas.add(new Bodega("123.45", "Descripcion 4", "Historia 1", "Bodega 3", 2, "2024-04-01", null, null));
-		listaBodegas.add(new Bodega("123.45", "Descripcion 5", "Historia 1", "Bodega 4", 2, "2023-04-01", null, null));
-		listaBodegas.add(new Bodega("123.45", "Descripcion 6", "Historia 1", "Bodega 5", 2, "2023-04-01", null, null));
+		listaBodegas.add(new Bodega("123.45", "Descripcion 2", "Historia 1", "Bodega 1", 4, "2021-04-01", null, null));
+		listaBodegas.add(new Bodega("123.45", "Descripcion 3", "Historia 1", "Bodega 2", 2, "2021-04-01", null, null));
+		listaBodegas.add(new Bodega("123.45", "Descripcion 4", "Historia 1", "Bodega 3", 2, "2021-04-01", null, null));
+		listaBodegas.add(new Bodega("123.45", "Descripcion 5", "Historia 1", "Bodega 4", 2, "2025-04-01", null, null));
+		listaBodegas.add(new Bodega("123.45", "Descripcion 6", "Historia 1", "Bodega 5", 2, "2025-04-01", null, null));
 		listaBodegas.add(new Bodega("123.45", "Descripcion 1", "Historia 1", "Bodega 6", 2, "2027-04-01", null, null));
 	}
 
@@ -81,14 +82,15 @@ public class ControladorImportarActualizacionVinosBodega {
 
 	//Metodos
 	public void opcionImportarActualizacionVinosBodega() {
+		PantallaAB pantalla = new PantallaAB();
 		List<String> listaBodegasConActualizacion = buscarBodegasParaActualizar(getListaBodegas());
 
-		//PREGUNTAR SOL
+
 		if (listaBodegasConActualizacion.isEmpty()){
 			JOptionPane.showMessageDialog(null, "No hay bodegas para actualizar");
 			//volver a inicio
 		} else {
-			PantallaAB.mostrarBodegasParaActualizar(listaBodegasConActualizacion);
+			pantalla.mostrarBodegasParaActualizar(listaBodegasConActualizacion);
 		}
 	}
 
@@ -106,10 +108,10 @@ public class ControladorImportarActualizacionVinosBodega {
 	}
 
 	public void tomarSeleccionBodega(List<String> bodegaSeleccionadas) {
-		List<Vino> vinosActualizados = getActualizacionVinos(bodegaSeleccionadas);
+		//List<Vino> vinosActualizados = getActualizacionVinos(bodegaSeleccionadas);
 	}
 
-	public List<Vino> getActualizacionVinos(List<String> bodegaSeleccionadas) {
+	public void getActualizacionVinos(List<String> bodegaSeleccionadas) {
 		//Aca nos quuedamos, falta hacer la api
 	}
 
